c55d74e5e0f28233d5be480c61def0f011a0a960
mario <mario@pc-mase>
2019-05-22 21:46:41 -0500

    agregue barreras y listado final en el main

--- src/buscadorclasificadorarchivos/BuscadorClasificadorArchivos.java
+++ src/buscadorclasificadorarchivos/BuscadorClasificadorArchivos.java
@@ -1,5 +1,7 @@
 package buscadorclasificadorarchivos;
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BuscadorClasificadorArchivos {
     public static void main(String[] args) {
@@ -13,6 +15,19 @@ public class BuscadorClasificadorArchivos {
         File archivo = new File(nomDirectorio);
         Buscador buscador = new Buscador(archivo,contador);
         buscador.start();
-        //System.out.println("TERMINÓ: main");
+        try {
+            buscador.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(
+                BuscadorClasificadorArchivos.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+        System.out.println(
+                    "=================================================");
+        System.out.println("main:\tDirectorios: "+contador[1]+","
+            +" Archivos: "+contador[0]);
+        System.out.println(
+                    "=================================================");
+        System.out.println("TERMINÓ: main");
     }
 }
