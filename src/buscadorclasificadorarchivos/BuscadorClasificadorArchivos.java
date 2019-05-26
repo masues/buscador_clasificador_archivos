0ab7346693aecb7cd84e09b2ae1c9bb463306d70
mario <mario@pc-mase>
2019-05-25 21:14:30 -0500

    adición de datos de entrada por el teclado

--- src/buscadorclasificadorarchivos/BuscadorClasificadorArchivos.java
+++ src/buscadorclasificadorarchivos/BuscadorClasificadorArchivos.java
@@ -14,14 +14,22 @@ public class BuscadorClasificadorArchivos {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduzca la ruta: ");
         nomDirectorio = teclado.nextLine();
-        System.out.println("Introduzca el número de elementos: ");
+        System.out.println("Introduzca el número de elementos a contar: ");
         numElementos = teclado.nextInt();
         
         int [] contador = new int[numElementos];
         
         File archivo = new File(nomDirectorio);
-        if(!archivo.exists()){
-            System.out.print("El directorio ingresado no existe\n"+
+        
+        //valida que sea una ruta correcta
+        if(!archivo.exists() || !archivo.isDirectory()){
+            System.out.print("El directorio ingresado no existe o no es un "+
+                    "directorio.\nTerminando ejecución...\n");
+            System.exit(0);
+        }
+        //valida que el numero de elementos sea 1 o 2
+        if(numElementos<1 || numElementos>2){
+            System.out.print("El número de elementos debe ser 1 o 2\n"+
                     "Terminando ejecución...\n");
             System.exit(0);
         }
@@ -35,8 +43,19 @@ public class BuscadorClasificadorArchivos {
         }
         
         System.out.println("=================================================");
-        System.out.println("Main: Directorios: "+contador[1]+",  Archivos: "+contador[0]);
+        imprimeContador(contador);
         System.out.println("=================================================");
         System.out.println("TERMINÓ: main");
+    }
+    public static void imprimeContador(int [] contador){
+        switch (contador.length){
+            case 1:
+                System.out.println("Main:\tArchivos: "+contador[0]);
+                break;
+            case 2:
+                System.out.println("Main: Directorios: "+contador[1]+
+                        ",  Archivos: "+contador[0]);
+                break;
+        }
     }
 }
